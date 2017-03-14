e7ad2152c3b50f90daf271fb3c4dc929ac1493d8
guguangzhu <574496377@qq.com>
2017-03-14 18:29:49 +0800

    图片选择器

--- myimagepicker/src/main/java/com/guge/myimagepicker/ui/GalleryActivity.java
+++ myimagepicker/src/main/java/com/guge/myimagepicker/ui/GalleryActivity.java
@@ -2,14 +2,31 @@ package com.guge.myimagepicker.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridView;
 
+import com.guge.myimagepicker.ImageDataSource;
 import com.guge.myimagepicker.R;
+import com.guge.myimagepicker.bean.ImageFolder;
 
-public class GalleryActivity extends AppCompatActivity {
+import java.util.List;
 
+public class GalleryActivity extends ImageBaseActivity implements ImageDataSource.OnImagesLoadedListener{
+
+    private GridView mGridView;  //图片展示控件
+    private View mFooterBar;     //底部栏
+    private Button mBtnOk;       //确定按钮
+    private Button mBtnDir;      //文件夹切换按钮
+    private Button mBtnPre;      //预览按钮
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
+    }
+
+    @Override
+    public void onImagesLoaded(List<ImageFolder> imageFolders) {
+
     }
 }
