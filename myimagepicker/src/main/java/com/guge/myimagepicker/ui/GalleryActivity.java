aa67107dece048e20e37b0194867ed3a613bf5b2
guugangzhu <574496377@qq.com>
2017-03-14 23:08:45 +0800

    adapter

--- myimagepicker/src/main/java/com/guge/myimagepicker/ui/GalleryActivity.java
+++ myimagepicker/src/main/java/com/guge/myimagepicker/ui/GalleryActivity.java
@@ -12,7 +12,7 @@ import com.guge.myimagepicker.bean.ImageFolder;
 
 import java.util.List;
 
-public class GalleryActivity extends ImageBaseActivity implements ImageDataSource.OnImagesLoadedListener{
+public class GalleryActivity extends ImageBaseActivity implements ImageDataSource.OnImagesLoadedListener, View.OnClickListener{
 
     private GridView mGridView;  //图片展示控件
     private View mFooterBar;     //底部栏
@@ -23,10 +23,24 @@ public class GalleryActivity extends ImageBaseActivity implements ImageDataSourc
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
+        findViewById(R.id.btn_back).setOnClickListener(this);
+        mBtnOk = (Button) findViewById(R.id.btn_ok);
+        mBtnOk.setOnClickListener(this);
+        mBtnDir = (Button) findViewById(R.id.btn_dir);
+        mBtnDir.setOnClickListener(this);
+        mBtnPre = (Button) findViewById(R.id.btn_preview);
+        mBtnPre.setOnClickListener(this);
+        mGridView = (GridView) findViewById(R.id.gridview);
+        mFooterBar = findViewById(R.id.footer_bar);
     }
 
     @Override
     public void onImagesLoaded(List<ImageFolder> imageFolders) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
 
     }
 }
