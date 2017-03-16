f45a536d7c894c6afd1400fecea53703228c9c50
guguangzhu <574496377@qq.com>
2017-03-16 18:33:13 +0800

    图片选择器

--- app/src/main/java/com/example/testandroid2/ImagePickerActivity.java
+++ app/src/main/java/com/example/testandroid2/ImagePickerActivity.java
@@ -3,9 +3,12 @@ package com.example.testandroid2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.transition.Fade;
+import android.transition.TransitionManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.guge.myimagepicker.ui.GalleryActivity;
 
@@ -19,13 +22,15 @@ import butterknife.OnClick;
  * 创建时间：2017/3/16 16:16
  */
 public class ImagePickerActivity extends BaseActivity {
-    public static final int CHOOSE_PIC=111;
+    public static final int CHOOSE_PIC = 111;
     @InjectView(R.id.button6)
     Button button6;
     @InjectView(R.id.button7)
     Button button7;
     @InjectView(R.id.imageView)
     ImageView imageView;
+    @InjectView(R.id.rl_root_view)
+    RelativeLayout rlRootView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -42,7 +47,17 @@ public class ImagePickerActivity extends BaseActivity {
                 startActivityForResult(intent, CHOOSE_PIC);
                 break;
             case R.id.button7:
+                TransitionManager.beginDelayedTransition(rlRootView, new Fade());
+//                toggleVisibility(button7, button7);
+                button7.setVisibility(View.INVISIBLE);
                 break;
+        }
+    }
+
+    private static void toggleVisibility(View... views) {
+        for (View view : views) {
+            boolean isVisible = view.getVisibility() == View.VISIBLE;
+            view.setVisibility(isVisible ? View.INVISIBLE : View.VISIBLE);
         }
     }
 }
